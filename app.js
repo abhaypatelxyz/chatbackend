e5b8cc460b938695d20e54bed9a5eebcff6608f4
ShubhamDaS-IIT-Dhanbad <shubhamdasiitcs@gmail.com>
2024-09-01 18:35:02 +0530

    Resolved merge conflicts

--- app.js
+++ app.js
@@ -1,61 +1,19 @@
-import express from 'express';
-import http from 'http';
-import cors from 'cors';
+import { app, server } from './index.js';
+import connectDB from './config/mongoDB.js';
+import setupSocket from './socket.js';
 
-// Create Express app
-const app = express();
-const server = http.createServer(app);
+setupSocket(server); // Setup Socket.IO with the server instance
 
-// CORS policy setup
-// CORS policy setup to allow all origins
-const corsOptions = {
-  origin: true, // Allow all origins
-  credentials: true,
-};
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Not allowed by CORS'));
-//     }
-//   },
-//   credentials: true,
-// };
+const port =process.env.PORT; // Allow port to be configured via environment variable
 
-app.use(cors(corsOptions));
-
-// Middleware to parse JSON
-app.use(express.json());
-
-// Basic route
-app.get('/', (req, res) => {
-  res.send("Hello here");
-});
-
-// Import routers
-import userRouter from './api/routes/userRouter.js';
-import chatRouter from './api/routes/chatRouter.js';
-import friendRouter from './api/routes/friendRouter.js';
-
-// Use routers
-app.use('/api', userRouter);
-app.use('/api', chatRouter);
-app.use('/api', friendRouter);
-
-// Middleware to handle JSON parsing errors
-app.use((err, req, res, next) => {
-  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
-    // Bad request due to invalid JSON
-    return res.status(400).json({ message: "Invalid JSON" });
-  }
-  next();
-});
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
-
-export { app, server };
+// Connect to MongoDB and start the server
+connectDB()
+  .then(() => {
+    server.listen(port, () => {
+      console.log(`Server is running at port: ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log('MongoDB connection failed:', err);
+    process.exit(1); // Exit with failure code
+  });
